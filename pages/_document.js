6e0dff8de0f0d05949537bb0c1c75203e0ab968e
Zufarbek <zufarbek.abdurakhmonov@gmail.com>
2023-12-25 00:35:49 +0500

    favi icon fixed

--- pages/_document.js
+++ pages/_document.js
@@ -9,8 +9,9 @@ class MyDocument extends Document {
                 href="https://fonts.bunny.net/css?family=noto-sans:400,500,700,800"
                 rel="stylesheet"
             />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
-
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+      
+          <meta property="og:image" content="/assets/imgs/soff study logo.png" />
         </Head>
         <body>
           <Main />
